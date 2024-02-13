98f465b875a867c3c93b4e699c34df0c3e35b447
“Rares <raresh.grecu@gmail.com>
2024-02-13 12:17:45 -0500

    Added NYPD_Arrest_Data__Year_to_Date__20240213

--- munge.py
+++ munge.py
@@ -1,1 +1,9 @@
 # place your code to clean up the data file below.
+
+import pandas as pd
+
+df = pd.read_csv('NYPD_Arrest_Data__Year_to_Date__20240213.csv') 
+
+df = df.drop(df.columns[2], axis=1)
+
+df.to_csv('modified_file.csv', index=False) 
